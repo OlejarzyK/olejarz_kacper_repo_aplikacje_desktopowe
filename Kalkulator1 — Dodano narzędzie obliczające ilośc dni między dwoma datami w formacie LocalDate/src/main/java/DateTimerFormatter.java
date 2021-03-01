dc7b7c9df8059668e8af1e10694135f4a1724e6a
OlejarzyK <77607422+OlejarzyK@users.noreply.github.com>
2021-03-01 08:42:28 +0100

    Add files via upload

---
+++ Kalkulator1 — Dodano narzędzie obliczające ilośc dni między dwoma datami w formacie LocalDate/src/main/java/DateTimerFormatter.java
@@ -0,0 +1,20 @@
+
+import java.time.format.DateTimeFormatter;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author G
+ */
+class DateTimerFormatter {
+
+    static DateTimeFormatter ofPattern(String dd_MM_yyyy) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
